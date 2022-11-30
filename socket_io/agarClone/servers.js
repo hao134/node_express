ae159ab189569f319d65a2ad8e9682dfa23cdcdf
hao134 <hao134@github.com>
2022-11-30 22:58:29 +0800

    add sockets and draw little orbs

--- socket_io/agarClone/servers.js
+++ socket_io/agarClone/servers.js
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 const socketio = require("socket.io");
 const expressServer = app.listen(8080);
-const io = socketio(expressServer);
+const io = socketio(expressServer, {
+  cors: {
+    origin: "*",
+  },
+});
 const helmet = require("helmet");
 app.use(helmet());
 console.log("Express and socketio are listening on port 8080");
